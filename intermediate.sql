f649e8e014bceb9f32618e5a597083d740f4a3f5
Julian G <juliancatorce@gmail.com>
2019-01-03 01:10:21 -0600

    Added: intermediate.sql

---
+++ intermediate.sql
@@ -0,0 +1,48 @@
+-- 1)Create database alumno
+CREATE TABLE PROFESSOR(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME VARCHAR(20) NOT NULL,
+LAST_NAME VARCHAR(20) NOT NULL, STREET_ADDRESS VARCHAR(50) NOT NULL, POSTAL_AREA INTEGER NOT NULL, 
+AGE INTEGER NOT NULL);
+
+-- 2) Drop table
+-- DROP TABLE ALUMNO;
+-- DELETE FROM ALUMNO WHERE ID==2;
+-- 3) Insert into
+-- INSERT INTO ALUMNO(NAME,LAST_NAME) VALUES("ALFONSO", "CABANAS", "PHOENIX 102",08200, 25 );
+-- INSERT INTO ALUMNO(NAME,LAST_NAME) VALUES("JUAN", "ZEPEDA", "PHOENIX 102",08200, 25);
+-- INSERT INTO ALUMNO(NAME,LAST_NAME) VALUES("LUCIANO", "ARREOLA", "PHOENIX 102",08200, 25);
+-- INSERT INTO ALUMNO(NAME,LAST_NAME) VALUES("JUSTINO", "DOMINGUEZ", "PHOENIX 102",08200, 25);
+-- INSERT INTO ALUMNO(NAME,LAST_NAME) VALUES("ALEXIS", "BUENROSTRO", "PHOENIX 102",08200, 25);
+-- INSERT INTO ALUMNO(NAME,LAST_NA E) VALUES("MANUELA", "MEJIA", "PHOENIX 102",08200, 25);
+-- INSERT INTO ALUMNO(NAME,LAST_NAME) VALUES("JOEL", "SOLARES", "PHOENIX 102",08200, 25);
+
+
+
+
+    -- 1) Constraints
+    --     * NOT NULL Constraint 
+    --     * DEFAULT Constraint 
+    --     * UNIQUE Constraint 
+    --     * PRIMARY Key 
+    --     * FOREIGN Key 
+    --     * CHECK Constraint 
+    --     * INDEX 
+    -- 2) Joins
+    -- 3) Unions Clause
+    -- 4) NULL Values
+    -- 5) Alias Syntax
+    -- 6) Indexes
+    -- 7) Alter Command
+    -- 8) Truncate Table
+    -- 9) Using Views
+    -- 10) Having Clause
+    -- 11) Transactions
+    -- 12) Wildcards
+    -- 13) Date Functions
+    -- 14) Temporary Tables
+    -- 15) Clone Tables
+    -- 16) Sub Queries
+    -- 17) Using Sequences
+    -- 18) Handling Duplicates
+    -- 19) Injection
+
+
